cbd5c5e6f081dfd5ffbdb95ed05493699ed0d176
Balázs Schmidt <balazs.schmidt2@stud.uni-corvinus.hu>
2021-11-14 22:08:08 +0100

    Add project files.

---
+++ MasodfokuEgyenlet/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace MasodfokuEgyenlet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_e1 = new System.Windows.Forms.TextBox();
+            this.textBox_e2 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(33, 24);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(340, 20);
+            this.textBoxA.TabIndex = 0;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(33, 60);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(340, 20);
+            this.textBoxB.TabIndex = 1;
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(33, 103);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(340, 20);
+            this.textBoxC.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 64);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Mehet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_e1
+            // 
+            this.textBox_e1.Location = new System.Drawing.Point(33, 260);
+            this.textBox_e1.Name = "textBox_e1";
+            this.textBox_e1.Size = new System.Drawing.Size(340, 20);
+            this.textBox_e1.TabIndex = 4;
+            // 
+            // textBox_e2
+            // 
+            this.textBox_e2.Location = new System.Drawing.Point(33, 296);
+            this.textBox_e2.Name = "textBox_e2";
+            this.textBox_e2.Size = new System.Drawing.Size(340, 20);
+            this.textBox_e2.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(393, 24);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(45, 20);
+            this.textBox6.TabIndex = 6;
+            this.textBox6.Text = "a:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(393, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(45, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "b:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(393, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(45, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "c:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox_e2);
+            this.Controls.Add(this.textBox_e1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_e1;
+        private System.Windows.Forms.TextBox textBox_e2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ MasodfokuEgyenlet/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasodfokuEgyenlet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a;
+            try
+            {
+                a = double.Parse(textBoxA.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Számot írj az 'a' paraméternek.");
+                return;
+            }
+
+            double b;
+            try
+            {
+                b = double.Parse(textBoxB.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Számot írj az 'b' paraméternek.");
+                return;
+            }
+
+            double c;
+            try
+            {
+                c = double.Parse(textBoxC.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Számot írj az 'c' paraméternek.");
+                return;
+            }
+
+
+
+            double d = Math.Pow(b, 2) - 4 * a * c;
+
+            double gyök1 = (-b - Math.Sqrt(d)) / (2 * a);
+            double gyök2 = (-b + Math.Sqrt(d)) / (2 * a);
+
+            textBox_e1.Text = gyök1.ToString();
+            textBox_e2.Text = gyök2.ToString();
+
+            if (d < 0)
+            {
+                textBox_e1.Visible = false;
+                textBox_e2.Visible = false;
+                return;
+            }
+            else
+            {
+                textBox_e1.Visible = true;
+                textBox_e2.Visible = true;
+            }
+
+
+            if (d == 0)
+            {
+                textBox_e2.Visible = false;
+            }
+            else
+            {
+                textBox_e2.Visible = true;
+            }
+        }
+    }
+}
